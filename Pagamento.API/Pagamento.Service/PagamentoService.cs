b8f5123ec3aaf24ea0178ed702ca42abb404ee6b
Layla <layla.joana@gmail.com>
2021-05-18 03:39:22 -0300

    [Pagamento-API]  - Criação do PagamentoService e Elaboração do Método AutorizarPagamento.

---
+++ Pagamento.API/Pagamento.Service/PagamentoService.cs
@@ -0,0 +1,20 @@
+﻿using Pagamento.Domain.Interfaces;
+using Pagamento.Domain.PagamentoDto;
+using Pagamento.Domain.PagamentoDtos;
+using System.Threading.Tasks;
+
+namespace Pagamento.Service
+{
+    public class PagamentoService : IPagamentoService
+    {
+        public async Task<PagamentoResponseDto> AutorizarPagamento(PagamentoRequestDto pagamentorequestDto)
+        {
+            var pagamentoresponse = new PagamentoResponseDto
+            {
+                Valor = pagamentorequestDto.Valor,
+                Estado = pagamentorequestDto.Valor > 100 ? "APROVADO" : "REJEITADO"
+            };
+            return pagamentoresponse;
+        }
+    }
+}
