5e2813385b302a4c9de28a31acefbb4f9317548c
Layla <layla.joana@gmail.com>
2021-05-18 03:45:03 -0300

    [Pagamento-API] - Criação do Pagamento.Application e PagamentoController e elaboração do Endpoint compras.

---
+++ Pagamento.API/Pagamento.Application/Controllers/PagamentoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pagamento.Domain.Interfaces;
+using Pagamento.Domain.PagamentoDto;
+using System.Threading.Tasks;
+
+namespace Pagamento.Application.Controllers
+{
+    /// <summary>
+    /// Controladora de Pagamentos.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagamentoController : ControllerBase
+    {
+        private readonly IPagamentoService _pagamentoService;
+
+        public PagamentoController(IPagamentoService pagamentoService)
+        {
+            _pagamentoService = pagamentoService;
+        }
+
+        /// <summary>
+        /// Autorizar um Pagamento.
+        /// </summary>
+        /// <param name="pagamentoRequest"></param>
+        /// <returns></returns>
+        [HttpPost("compras")]
+        public async Task<ActionResult> AutorizarPagamento(PagamentoRequestDto pagamentoRequest)
+        {
+            try
+            {
+                var pagamentoResponse = await _pagamentoService.AutorizarPagamento(pagamentoRequest);
+                return Ok(pagamentoResponse);
+            }
+            catch
+            {
+                return BadRequest("Ocorreu um erro desconhecido.");
+            }
+        }
+    }
+}
---
+++ Pagamento.API/Pagamento.Application/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Pagamento.Infra.Mapping;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Pagamento.Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Documentação API ",
+                    Version = "v1",
+                    Description = "Desafio Na Pista: API REST Pagamento.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Layla Joana Santos",
+                        Url = new Uri("https://github.com/LaylaJoana")
+                    }
+                });
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.SetupMappingLayer(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseDeveloperExceptionPage();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+
+    }
+}
