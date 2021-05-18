c87d2093301a2159d093a2aca2c8c491e6eb9d1a
Layla <layla.joana@gmail.com>
2021-05-18 03:41:30 -0300

    [Pagamento-API] - Criação do Pagamento.Infra.Mapping.

---
+++ Pagamento.API/Pagamento.Infra.Mapping/ServiceExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pagamento.Domain.Interfaces;
+using Pagamento.Service;
+
+namespace Pagamento.Infra.Mapping
+{
+    public static  class ServiceExtension
+    {
+        public static IServiceCollection SetupMappingLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IPagamentoService, PagamentoService>();
+      
+            return services;
+        }
+    }
+}
